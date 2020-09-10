8136d0ec710e96a120632904f6afb6adfe28690a
Ajay20062 <ajaydharrsan@gmail.com>
2020-09-10 17:45:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-kzvykq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-kzvykq)
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('hello is me !')
+<body>
+    <h2>oh whaaa!</h2>
+    <a href="/https://www.youtube.com/watch?v=_UWrpL3cjFo">My other page</a>
+  </body>
